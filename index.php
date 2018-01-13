ee7cda0681c87f33d1b9abe8e7bae766816d0e9e
Brian Hernandez <brian.hernandez@globant.com>
2018-01-13 03:45:22 -0600

    new favicon

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
   <script src="js/respond.min.js"></script>
 <![endif]-->
 
-<link rel="shortcut icon" href="https://demo.web3canvas.com/themeforest/startupkit/images/favicon.png">
+<link rel="shortcut icon" href="images/minutos-felices.png">
 <script src="js/pace.js"></script>
 
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,600' rel='stylesheet' type='text/css'>
@@ -60,16 +60,6 @@
 </div>
 </div>
 
-
-<!--<a href="index.html#explore">
-<p>SEGUIR EXPLORANDO</p>
-<p class="scrollto--arrow"><img src="images/scroll_down.png" alt="scroll down arrow"></p>
-</a>-->
-
-
-
-
-
 </div>
 
 <section id="section03" class="demo">
@@ -77,24 +67,6 @@
 </section>
 
 </main>
-
-
-
-
-<!--<div class="container">
-<div class="section-title">
-<h2>Llevamos la magia <strong>de tu boda</strong>  al mundo digital</h2>
-<h4>Durante 30 minutos le damos un toque <span data-typer-targets="emotivo, especial, moderno"></span> a tu evento</h4>
-
-
-</div> 
-</div>-->
-
-
-
-
-
-
 
 <div class="container" >
 
@@ -134,19 +106,7 @@ y fotos desde su smartphone utilizando minutos felices</p>
 
 </section>
 
-
-<!--<div class="section-title">
-<h2>You are in a good company</h2>
-<h4>We are funded with world's trusted brands. </h4>
 </div>
-<section class="row clientlogo breath">
-<div class="col-md-12 text-center"> <img src="images/client-logos.png" alt="client-logos"></div>
-</section> -->
-
-</div>
-
-
-
 
 <div class="container" id="explore">
 
@@ -167,87 +127,6 @@ y fotos desde su smartphone utilizando minutos felices</p>
 </div>
 
 </div> <!--Container -->
-
-
-
-
-<!--<div class="highlight testimonials">
-<div class="container">
-
-<div class="section-title">
-<h2>What our customers said</h2>
-<h4>Don't take our word. See our testimonials </h4>
-</div>
-<section class="row breath">
-<div class="col-md-6">
-<div class="testblock">Startup is really a great template to get strarted with. With its awesome features and easy customization with unbounce. This template made my day. Very recommended.</div>
-<div class="clientblock"> <img src="images/customer-img-1.jpg" alt=".">
-<p><strong>Johnathan Doe</strong> <br>
-Founder and CEO of <a href="index-v-signup.html#">Square Inc</a></p>
-</div>
-</div>
-<div class="col-md-6">
-<div class="testblock">I like this template very much. easy customization and perfect support. This is the finest startup template I've ever seen. Its minimal, Flat and responsive too. I'm really happy with their service.</div>
-<div class="clientblock"> <img src="images/customer-img-2.jpg" alt=".">
-<p><strong>Michael Slats</strong> <br>
-Project Lead at <a href="index-v-signup.html#">Airbnb.com</a></p>
-</div>
-</div>
-</section>
-
-</div>
-</div> -->
-
-
-
-
-<!--<div class="section-title">
-<h2>Honest pricing. No surprises.</h2>
-<h4>No contract. No risk. No credit card required. </h4>
-</div>
-<section class="row breath planpricing">
-<div class="col-md-4">
-<div class="pricing color1">
-<div class="planname">Personal</div>
-<div class="price"> <span class="curr">$</span>14<span class="per">/MO</span></div>
-<div class="billing">Billed Annually</div>
-</div>
-</div>
-<div class="col-md-4">
-<div class="pricing color2">
-<div class="planname">Agency</div>
-<div class="price"> <span class="curr">$</span>49<span class="per">/MO</span></div>
-<div class="billing">Billed Annually</div>
-</div>
-</div>
-<div class="col-md-4">
-<div class="pricing color3">
-<div class="planname">Enterprise</div>
-<div class="price"> <span class="curr">$</span>99<span class="per">/MO</span></div>
-<div class="billing">Billed Annually</div>
-</div>
-</div>
-</section> -->
-
-
-<!--<div class="section-title">
-<h5>Frequently Asked Questions</h5>
-</div>
-<section class="row faq breath">
-<div class="col-md-6">
-<h6>How does the free trial work?</h6>
-<p>Our 10 day trial is 100% free and does not require credit card information to start. If at the end of your trial you would like to upgrade, great. If not, you can cancel your account altogether, or let us mark it as inactive for you to come back to later.</p>
-<h6>Can I switch plans later?</h6>
-<p>Absolutely. You can switch between our paid plans, or cancel your account altogether, whenever you like. We will adjust any payments accordingly.</p>
-</div>
-<div class="col-md-6">
-<h6>Do I need to choose a plan now?</h6>
-<p>No. You get the full featured, unlimited version of Startup completely free for 10 days. Once you're ready to upgrade, you may choose a plan which suits your needs.</p>
-<h6>What payment types do you accept?</h6>
-<p>We accept payments from MasterCard, Visa, Visa Debit and American Express. We do not accept PayPal. Remember, you do not need to supply card details to start your free trial.</p>
-</div>
-</section> -->
-
 
 
 <main class="footercta" role="main">
