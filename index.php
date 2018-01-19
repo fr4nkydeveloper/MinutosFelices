b54989f4bacf8bbd3f336e71d5b38c96b12ea10b
Frank Andrade <frank.andrade.10@gmail.com>
2018-01-18 19:10:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 </script>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Bodas, esposos, felicitaciones">
+<meta name="description" content="Sorprende a tus invitados y déjate consentir con minutos felices">
 <title>Minutos Felices</title>
 
 <link href="css/bootstrap.css" rel="stylesheet"> 
@@ -20,7 +20,7 @@
 <link href="css/alertify.min.css" rel="stylesheet">
 <link href="css/startup.css" rel="stylesheet">
 <link href="css/sweetalert.min.css" rel="stylesheet">
-
+<link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet"> 
 <!--[if lt IE 9]>
   <script src="js/html5shiv.js"></script>
   <script src="js/respond.min.js"></script>
@@ -31,17 +31,93 @@
 
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,600' rel='stylesheet' type='text/css'>
 </head>
-<body class="signup-v-layout">
-	<div class="preloader"></div>
+<body>
 
-<main id="top" class="masthead" role="main">
+
+<div class="logo"> 
+<img src="images/logo.png" alt="startup-logo" width="223" height="99"> 
+</div>
+
+
+<header>
+  <div class="slider">
+  <div class="slide_viewer">
+    <div class="slide_group">
+      <div class="slide">
+      <div class="copySlide">
+        <h1>Sorprende a tus invitados y déjate consentir</h1>
+        <h3>Plataforma para felicitaciones y fotos</h3>
+        <button type="button" class="btn btnCTA" data-toggle="modal" data-target="#myModal">
+        Lo quiero en mi fiesta
+</button>  
+      </div>
+      </div>
+      <div class="slide">
+        <div class="copySlide">
+          <h1>Dale vida a la pantalla de tu evento</h1>
+          <h3>Se formará un collage con las felicitaciones y fotos que suban tus invitados</h3>
+          <button type="button" class="btn btnCTA" data-toggle="modal" data-target="#myModal">
+        Lo quiero en mi fiesta
+</button>  
+        </div>
+      </div>
+      <div class="slide">
+        <div class="copySlide">
+          <h1>Momentos inolvidables</h1>
+          <h3>Recibe un hermoso álbum con el contenido generado</h3>
+          <button type="button" class="btn btnCTA" data-toggle="modal" data-target="#myModal">
+         Lo quiero en mi fiesta
+</button>  
+        </div>
+      </div>
+    </div>
+  </div>
+</div><!-- End // .slider -->
+
+
+
+
+<div class="slide_buttons">  
+</div>
+
+
+<div class="directional_nav">
+  <div class="previous_btn" title="Previous">
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="65px" height="65px" viewBox="-11 -11.5 65 66">
+      <g>
+        <g>
+          <path fill="#474544" d="M-10.5,22.118C-10.5,4.132,4.133-10.5,22.118-10.5S54.736,4.132,54.736,22.118
+      c0,17.985-14.633,32.618-32.618,32.618S-10.5,40.103-10.5,22.118z M-8.288,22.118c0,16.766,13.639,30.406,30.406,30.406 c16.765,0,30.405-13.641,30.405-30.406c0-16.766-13.641-30.406-30.405-30.406C5.35-8.288-8.288,5.352-8.288,22.118z"/>
+          <path fill="#474544" d="M25.43,33.243L14.628,22.429c-0.433-0.432-0.433-1.132,0-1.564L25.43,10.051c0.432-0.432,1.132-0.432,1.563,0 c0.431,0.431,0.431,1.132,0,1.564L16.972,21.647l10.021,10.035c0.432,0.433,0.432,1.134,0,1.564  c-0.215,0.218-0.498,0.323-0.78,0.323C25.929,33.569,25.646,33.464,25.43,33.243z"/>
+        </g>
+      </g>
+    </svg>
+  </div>
+  <div class="next_btn" title="Next">
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="65px" height="65px" viewBox="-11 -11.5 65 66">
+      <g>
+        <g>
+          <path fill="#474544" d="M22.118,54.736C4.132,54.736-10.5,40.103-10.5,22.118C-10.5,4.132,4.132-10.5,22.118-10.5  c17.985,0,32.618,14.632,32.618,32.618C54.736,40.103,40.103,54.736,22.118,54.736z M22.118-8.288  c-16.765,0-30.406,13.64-30.406,30.406c0,16.766,13.641,30.406,30.406,30.406c16.768,0,30.406-13.641,30.406-30.406 C52.524,5.352,38.885-8.288,22.118-8.288z"/>
+          <path fill="#474544" d="M18.022,33.569c 0.282,0-0.566-0.105-0.781-0.323c-0.432-0.431-0.432-1.132,0-1.564l10.022-10.035      L17.241,11.615c 0.431-0.432-0.431-1.133,0-1.564c0.432-0.432,1.132-0.432,1.564,0l10.803,10.814c0.433,0.432,0.433,1.132,0,1.564 L18.805,33.243C18.59,33.464,18.306,33.569,18.022,33.569z"/>
+        </g>
+      </g>
+    </svg>
+  </div>
+</div><!-- End // .directional_nav -->
+</header>
+  
+
+
+
+
+
+
+
+
+<!--<main id="top" class="masthead" role="main">
 <div class="container">
 <div class="logo"> <a href="index-v-signup.html#"><img src="images/logo.png" alt="startup-logo" width="223" height="99"></a> </div>
 
-<!--<h1>Agregale 30 minutos <span data-typer-targets="divertidos,modernos,emotivos">emotivos</span> <strong>a tu boda</strong> </h1>-->
-<!--<h3>  </h3> -->
-<!--<h1 class="tituloPrincipal">Ponle la cereza del pastel a tu boda</h1>
-<h3>Sorprende a tus invitados y recibe un regalo increíble</h3>-->
 <h2 class="tituloHeader">Llevamos la magia <strong>de tu boda</strong>  al mundo digital</h2>
 <h3>Durante 30 minutos le damos un toque <span data-typer-targets="emotivo, especial, moderno"></span> a tu evento</h3>
 
@@ -74,22 +150,73 @@
   <a href="#caracteristicasProducto" id="featuresProducto"><span></span></a>
 </section>
 
-</main>
+</main> -->
 
-<div class="container" >
+<div class="container explicacionLead" >
 
-<div class="section-title">
-<h2 id="caracteristicasProducto">¿Cómo funciona minutos felices?</h2>
-<h4>Activamos la web-app en el evento durante 30 minutos y la magia comienza a suceder. </h4>
-</div>
 <section class="row features">
+
+<h4 class="text-center howItWorks">Prueba lo sencillo que es minutos felices</h4>
+
+<div class="col-sm-6 col-md-4">
+<div class="thumbnail"> 
+<div class="caption">
+<h3 class="captionTitulo">1. Regístrate</h3> <i class="fa fa-envelope-o iconWorks" aria-hidden="true"></i>
+<p class="description-features descriptionEasy"> Tu decides si platicamos por mail, inbox o whats</p>
+</div>
+</div>
+</div>
+
+<div class="col-sm-6 col-md-4 thumbnailSpecial">
+<div class="thumbnail">
+<div class="caption">
+<h3 class="captionTitulo">2. Seguimiento</h3> <i class="fa fa-whatsapp iconWorks2" aria-hidden="true"></i>
+<p class="description-features descriptionEasy ">
+En tan solo unos minutos nos ponemos de acuerdo para los detalles que 
+necesitamos de tu evento. Despues nos encargamos del resto</p>
+</div>
+</div>
+</div>
+
+<div class="col-sm-12 col-md-4 thumbnailSpecial">
+<div class="thumbnail"> 
+<div class="caption">
+<h3 class="captionTitulo">3. Disfruten de su fiesta</h3> <i class="iconWorks fa fa-heart-o" aria-hidden="true"></i>
+<p class="description-features descriptionEasy">Activamos remotamente la plataforma el día del evento ;)
+ </p>
+</div>
+</div>
+</div>
+
+</section>
+
+<div class="explicacionBtn">
+  <button type="submit" class="btn btnCTA" data-toggle="modal" data-target="#myModal"> Lo quiero en mi fiesta</button>
+</div>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+<div class="container containerHowItWorks" >
+
+<section class="row features">
+
+<h4 class="text-center howItWorks">¿Cómo funciona minutos felices?</h4>
 
 <div class="col-sm-6 col-md-4">
 <div class="thumbnail"> <img src="images/flat-feature-icon-1.png" alt="analytics-icon">
 <div class="caption">
-<h3>1. Recibe amor</h3>
+<h3 class="captionTitulo">Recibe amor</h3>
 <p class="description-features"> Tus invitados suben felicitaciones 
-y fotos desde su smartphone utilizando minutos felices</p>
+y fotos durante 30 minutos desde su smartphone</p>
 </div>
 </div>
 </div>
@@ -97,7 +224,7 @@ y fotos desde su smartphone utilizando minutos felices</p>
 <div class="col-sm-6 col-md-4">
 <div class="thumbnail"> <img src="images/flat-feature-icon-2.png" alt="analytics-icon">
 <div class="caption">
-<h3>2. Sorprendete</h3>
+<h3 class="captionTitulo">Sorprendete</h3>
 <p class="description-features">Las felicitaciones y fotos se van mostrando en la pantalla principal del evento formando un collage espectacular</p>
 </div>
 </div>
@@ -106,7 +233,7 @@ y fotos desde su smartphone utilizando minutos felices</p>
 <div class="col-sm-12 col-md-4">
 <div class="thumbnail"> <img src="images/flat-feature-icon-3.png" alt="analytics-icon">
 <div class="caption">
-<h3>3. Te mandamos tu regalo</h3>
+<h3 class="captionTitulo">Te mandamos tu regalo</h3>
 <p class="description-features">Llegará a tu casa un libro impreso personalizado con las felicitaciones y fotos generadas desde minutos felices </p>
 </div>
 </div>
@@ -114,40 +241,102 @@ y fotos desde su smartphone utilizando minutos felices</p>
 
 </section>
 
+<div class="explicacionBtn">
+  <button type="submit" class="btn btnCTA"  data-toggle="modal" data-target="#myModal"> Lo quiero en mi fiesta
+  </button>
 </div>
 
-<div class="container" id="explore">
 
-<div class="flexApp">
-
-<div class="app">
-<img src="images/app1.jpg" class="animated infinite bounce">  
-</div>
-
-<div class="tv">  
-<img src="images/app2.jpg" class="animated infinite pulse">  
-</div>
-
-<div class="book">
-<img src="images/app3.jpg" class="animated infinite swing">  
-</div>
 
 </div>
 
-</div> <!--Container -->
 
 
-<main class="footercta" role="main">
+
+
+<main class="footercta" role="main" style="margin-top: 20px;">
 <div class="container">
 
-<h1>We love <strong>weddings</strong> <br>
-and we love <strong>technology</strong></h1>
+<h2 class="tituloHeader">Llevamos la magia <strong>de tu boda</strong>  al mundo digital</h2>
+<h3>Durante 30 minutos le damos un toque <span data-typer-targets="emotivo, especial, moderno"></span> a tu evento</h3>
+
 
 <div class="row">
-<div class="col-md-12 breath text-center"> <a href="#top" class="btn btn-success btn-lg" id="footerBtn">PRUEBALO GRATIS</a> </div>
+<div class="col-md-12 breath text-center"> 
+<!-- Button trigger modal -->
+<button type="button" class="btn btnCTA" data-toggle="modal" data-target="#myModal">
+   Lo quiero en mi fiesta
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" 
+     aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <button type="button" class="close" 
+                   data-dismiss="modal">
+                       <span aria-hidden="true">&times;</span>
+                       <span class="sr-only">Close</span>
+                </button>
+                <h4 class="modal-title titleModal" id="myModalLabel">
+                    Disfruta GRATIS minutos felices ;)
+                </h4>
+            </div>
+            
+            <!-- Modal Body -->
+            <div class="modal-body">
+                
+          <form role="form" method="POST" name="registration" id="registerForm"
+          action="https://minutosfelices.us17.list-manage.com/subscribe/post?u=c32564ffba00eca03bc95243f&amp;id=4c2b23c5d0">
+        <div class="form-group">
+        <input class="form-control inputBoda" name="NOMBRE" type="text" placeholder="Nombre">
+        </div>
+
+        <div class="form-group">
+        <input class="form-control inputBoda" name="EMAIL" type="email" placeholder="Email" >
+        </div>
+        <div class="form-group">
+        <input class="form-control inputBoda" name="PHONE" type="tel" placeholder="Teléfono" >
+        </div> 
+
+
+        <!--<div class="form-group">
+        <label class="labelPreguntaBoda">¿Te casas este año?</label>
+         <input class="" type="radio" name="boda" value="Si" checked> Sí <br>
+         <input class="" type="radio" name="boda" value="No"> No <br>
+        </div>-->
+              
+                
+            </div>
+            
+            <!-- Modal Footer -->
+            <div class="modal-footer">
+                <div class="form-group last btnRegistroContainer">
+                  <button type="submit" class="btn btnRegistro">Regístrate</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</div>
 </div>
 </div>
 </main>
+
+
+<!--<h2 class="tituloHeader">Llevamos la magia <strong>de tu boda</strong>  al mundo digital</h2>
+<h3>Durante 30 minutos le damos un toque <span data-typer-targets="emotivo, especial, moderno"></span> a tu evento</h3>
+
+
+<div class="explicacionBtn">
+  <button type="submit" class="btn btn-success ">PRUEBALO GRATIS</button>
+</div>-->
+
+
 
 <script src="js/jquery.js"></script>
 <script src="js/bootstrap.js"></script>
@@ -160,6 +349,99 @@ and we love <strong>technology</strong></h1>
 
 <script>
 
+
+    $(document).ready(function(){
+$('.slider').each(function() {
+  var $this = $(this);
+  var $group = $this.find('.slide_group');
+  var $slides = $this.find('.slide');
+  var bulletArray = [];
+  var currentIndex = 0;
+  var timeout;
+  
+  function move(newIndex) {
+    var animateLeft, slideLeft;
+    
+    advance();
+    
+    if ($group.is(':animated') || currentIndex === newIndex) {
+      return;
+    }
+    
+    bulletArray[currentIndex].removeClass('active');
+    bulletArray[newIndex].addClass('active');
+    
+    if (newIndex > currentIndex) {
+      slideLeft = '100%';
+      animateLeft = '-100%';
+    } else {
+      slideLeft = '-100%';
+      animateLeft = '100%';
+    }
+    
+    $slides.eq(newIndex).css({
+      display: 'block',
+      left: slideLeft
+    });
+    $group.animate({
+      left: animateLeft
+    }, function() {
+      $slides.eq(currentIndex).css({
+        display: 'none'
+      });
+      $slides.eq(newIndex).css({
+        left: 0
+      });
+      $group.css({
+        left: 0
+      });
+      currentIndex = newIndex;
+    });
+  }
+  
+  function advance() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      if (currentIndex < ($slides.length - 1)) {
+        move(currentIndex + 1);
+      } else {
+        move(0);
+      }
+    }, 5500);
+  }
+  
+  $('.next_btn').on('click', function() {
+    if (currentIndex < ($slides.length - 1)) {
+      move(currentIndex + 1);
+    } else {
+      move(0);
+    }
+  });
+  
+  $('.previous_btn').on('click', function() {
+    if (currentIndex !== 0) {
+      move(currentIndex - 1);
+    } else {
+      move(3);
+    }
+  });
+  
+  $.each($slides, function(index) {
+    var $button = $('<a class="slide_btn">&bull;</a>');
+    
+    if (index === currentIndex) {
+      $button.addClass('active');
+    }
+    $button.on('click', function() {
+      move(index);
+    }).appendTo('.slide_buttons');
+    bulletArray.push($button);
+  });
+  
+  advance();
+});
+    });
+
     $(function () {
       $('[data-typer-targets]').typer();
     });
@@ -171,12 +453,6 @@ and we love <strong>technology</strong></h1>
 	  });
 	});
 
-    $(function() {
-	  $('#footerBtn').on('click', function(e) {
-		e.preventDefault();
-		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 700, 'linear');
-	  });
-	});
 
 </script>
 
@@ -197,6 +473,9 @@ $(document).ready(function(){
       EMAIL: {
         required: true,
         email: true
+      },
+      PHONE: {
+        required: true
       }
     }, 
 
@@ -206,7 +485,10 @@ $(document).ready(function(){
       },
       EMAIL: {
         required: "Ingresa un email válido"
-      }
+      },
+      PHONE: {
+        required: "Ingresa un teléfono  válido"
+      } 
     },
 
 
@@ -233,3 +515,4 @@ $(document).ready(function(){
 
 </body>
 </html>
+
